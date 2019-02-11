7f29eff58de675302e4d0c2afbb4fc657b1baa9c
Colugnat <alessandro.colugnat@samtrevano.ch>
2019-02-11 10:16:07 +0100

    no message

---
+++ Codice/ProgettoGiocoCarteIA - Backup/IntelligenzaArtificiale/View/MainView.xaml.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IntelligenzaArtificiale.View
+{
+    /// <summary>
+    /// Interaction logic for MainView.xaml
+    /// </summary>
+    public partial class MainView : UserControl
+    {
+        private int countTab = -1;
+        private String tab = "";
+        private String[] cardInGame = null;
+        private String[] cardInGameCopy = null;
+        private String[][] cardAI = null;
+        private String[][] cardAICopy = null;
+        private List<String> combinations = new List<String>();
+        private List<String>[] saveCombinations = null;
+        private int addCard = 0;
+
+        public MainView()
+        {
+            cardInGame = new String[2];
+            cardAI = new String[7][];
+            saveCombinations = new List<string>[10];
+            InitializeComponent();
+            for(int i = 0; i < 7; i++)
+            {
+                Thread.Sleep(50);
+                cardAI[i] = new String[2];
+                RandomCardGenerator(i);
+            }
+            carta0.Text = cardAI[0][0] + " " + cardAI[0][1];
+            carta1.Text = cardAI[1][0] + " " + cardAI[1][1];
+            carta2.Text = cardAI[2][0] + " " + cardAI[2][1];
+            carta3.Text = cardAI[3][0] + " " + cardAI[3][1];
+            carta4.Text = cardAI[4][0] + " " + cardAI[4][1];
+            carta5.Text = cardAI[5][0] + " " + cardAI[5][1];
+            carta6.Text = cardAI[6][0] + " " + cardAI[6][1];
+            cardAICopy = (String[][])cardAI.Clone();
+            cardInGameCopy = (String[])cardInGame.Clone();
+        }
+        private void RandomCardGenerator(int i)
+        {
+            Random rnd = new Random();
+            int colorNum = rnd.Next(0, 4);
+            switch (colorNum)
+            {
+                case 0:
+                    cardAI[i][0] = "Rosso";
+                    break;
+                case 1:
+                    cardAI[i][0] = "Giallo";
+                    break;
+                case 2:
+                    cardAI[i][0] = "Verde";
+                    break;
+                case 3:
+                    cardAI[i][0] = "Blu";
+                    break;
+                default:
+                    break;
+            }
+            cardAI[i][1] = rnd.Next(0, 10).ToString();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (color0.IsChecked.Value)
+                cardInGame[0] = color0.Content.ToString();
+            else if(color1.IsChecked.Value)
+                cardInGame[0] = color1.Content.ToString();
+            else if (color2.IsChecked.Value)
+                cardInGame[0] = color2.Content.ToString();
+            else if (color3.IsChecked.Value)
+                cardInGame[0] = color3.Content.ToString();
+
+            cardInGame[1] = number.Value.ToString();
+
+           cardGame.Text = cardInGame[0] + " " + cardInGame[1];
+           SelectGame(cardInGame[0], cardInGame[1], false);
+        }
+        private String AddTab(bool remove)
+        {
+            tab = "";
+            if (remove)
+                countTab += 1;
+            else
+                countTab -= 1;
+            for(int i = 0; i < countTab; i++)
+            {
+                tab += " L ";
+            }
+            return tab;
+        }
+        private void Save()
+        {
+            if(combinations.Count !=  0)
+            {
+                addCard += 1;
+                saveCombinations[addCard - 1] = combinations.ToList();
+            }
+        }
+        private void SelectGame(String selectedCard, String selectedNum, bool nextIsMy)
+        {
+            for(int i = 0; i < cardAICopy.Length; i++)
+            {
+                if (((cardAICopy[i][0] == selectedCard) && !nextIsMy) || (cardAICopy[i][1] == selectedNum))
+                {
+                    cardsMove.Text += AddTab(true) + cardAICopy[i][0] + " " + cardAICopy[i][1] + Environment.NewLine;
+                    cardInGameCopy[0] = cardAICopy[i][0];
+                    cardInGameCopy[1] = cardAICopy[i][1];
+                    combinations.Add(cardInGameCopy[0] + " " + cardInGameCopy[1]);
+                    cardAICopy[i][0] = "";
+                    cardAICopy[i][1] = "";
+                    SelectGame(cardInGameCopy[0], cardInGameCopy[1], true);
+                    cardAICopy[i][0] = cardAI[i][0];
+                    cardAICopy[i][1] = cardAI[i][1];
+                    AddTab(false);
+                }
+            }
+            Save();
+            combinations.RemoveRange(0, combinations.Count);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            cardsMove.Text = "";
+            for (int i = 0; i < 7; i++)
+            {
+                Thread.Sleep(50);
+                cardAI[i] = new String[2];
+                RandomCardGenerator(i);
+            }
+            carta0.Text = cardAI[0][0] + " " + cardAI[0][1];
+            carta1.Text = cardAI[1][0] + " " + cardAI[1][1];
+            carta2.Text = cardAI[2][0] + " " + cardAI[2][1];
+            carta3.Text = cardAI[3][0] + " " + cardAI[3][1];
+            carta4.Text = cardAI[4][0] + " " + cardAI[4][1];
+            carta5.Text = cardAI[5][0] + " " + cardAI[5][1];
+            carta6.Text = cardAI[6][0] + " " + cardAI[6][1];
+            cardAICopy = (String[][])cardAI.Clone();
+            cardInGameCopy = (String[])cardInGame.Clone();
+        }
+    }
+}
--- Codice/ProgettoGiocoCarteIA/ProgettoGiocoCarteIA/View/MainView.xaml.cs
+++ Codice/ProgettoGiocoCarteIA/ProgettoGiocoCarteIA/View/MainView.xaml.cs
@@ -7,16 +7,15 @@ using System.Windows;
 using System.Windows.Controls;
 using WebEye.Controls.Wpf;
 
-using AForge;
-using AForge.Imaging;
-using AForge.Imaging.Filters;
-using AForge.Math.Geometry;
 using System;
 using System.Threading;
 using Patagames.Ocr;
 using Patagames.Ocr.Enums;
 using System.Linq;
 using System.Windows.Media.Imaging;
+using ObjectRecognition;
+using System.Windows.Interop;
+using System.Windows.Media;
 using System.IO;
 
 namespace ProgettoGiocoCarteIA.View
@@ -26,17 +25,136 @@ namespace ProgettoGiocoCarteIA.View
     /// </summary>
     public partial class MainView : UserControl
     {
-        public const int COUNTNUMCARD = 50;
-        public const int MSECONDNUMCARD = 200;
+        //public const int COUNTNUMCARD = 50;
+        //public const int MSECONDNUMCARD = 100;
         public MainView()
         {
             InitializeComponent();
             InitializeComboBox();
         }
 
+        public BitmapImage Convert(System.Drawing.Image img)
+        {
+            using (var memory = new MemoryStream())
+            {
+                img.Save(memory, ImageFormat.Png);
+                memory.Position = 0;
+
+                var bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+
+                return bitmapImage;
+            }
+        }
+
+        private BitmapImage learningImage = null;
+        private BitmapImage backgroundImage = null;
+        private BitmapImage identificationImage = null;
+
+        private void LearningImageOneButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            backgroundImage = Convert(LearningImageOne.GetCurrentImage());
+        }
+
+        private void LearningImageTwoButton_Click(object sender, RoutedEventArgs e)
+        {
+            learningImage = Convert(LearningImageOne.GetCurrentImage());
+            this.LearningImageTwo.Source = learningImage;
+        }
+
+        private void AnalyzeImageButton_Click(object sender, RoutedEventArgs e)
+        {
+            identificationImage = Convert(LearningImageOne.GetCurrentImage());
+            this.AnalysisImage.Source = identificationImage;
+        }
+
+        private void LearnObjectButton_Click(object sender, RoutedEventArgs e)
+        {
+            string objectName = this.ObjectNameText.Text;
+            if (objectName.Length == 0)
+            {
+                MessageBox.Show("Please give the item a name");
+            }
+            else if (learningImage == null || backgroundImage == null)
+            {
+                MessageBox.Show("Please select a background image and an image with the object to learn");
+            }
+            else
+            {
+                ObjectLearningServices.LearnObject(learningImage, backgroundImage, objectName);
+                PopulateLearnedItemsList();
+            }
+        }
+
+        private void AnalyzeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (identificationImage == null)
+            {
+                MessageBox.Show("Please select an image to analyze");
+            }
+            else
+            {
+                IList<string> identifiedObjects = ObjectIdentificationService.AnalyzeImage(identificationImage);
+                this.AnalysisResultsText.Text = "";
+
+                foreach (string objectName in identifiedObjects)
+                {
+                    this.AnalysisResultsText.Text = this.AnalysisResultsText.Text + ("May contain " + objectName + "\r\n");
+                }
+
+                if (identifiedObjects.Count == 0)
+                {
+                    this.AnalysisResultsText.Text = "Did not recognize anything";
+                }
+            }
+        }
+
+        private void Window_Loaded_1(object sender, RoutedEventArgs e)
+        {
+            PopulateLearnedItemsList();
+        }
+
+        private void DeleteObjectButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.LearnedObjectsList.SelectedIndex != -1)
+            {
+                string objectName = (string)this.LearnedObjectsList.SelectedItem;
+                ObjectMemoryService.RemoveSignatureByName(objectName);
+                PopulateLearnedItemsList();
+            }
+            else
+            {
+                MessageBox.Show("Please select an item to delete");
+            }
+        }
+
+        private void PopulateLearnedItemsList()
+        {
+            List<ObjectSignatureData> learnedObjects = ObjectMemoryService.GetSignatures();
+            this.LearnedObjectsList.Items.Clear();
+            foreach (ObjectSignatureData objectSignatureData in learnedObjects)
+            {
+                this.LearnedObjectsList.Items.Add(objectSignatureData.ObjectName);
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
         private void InitializeComboBox()
         {
-            comboBox.ItemsSource = webCameraControl.GetVideoCaptureDevices();
+            comboBox.ItemsSource = LearningImageOne.GetVideoCaptureDevices();
 
             if (comboBox.Items.Count > 0)
             {
@@ -52,101 +170,125 @@ namespace ProgettoGiocoCarteIA.View
         private void OnStartButtonClick(object sender, RoutedEventArgs e)
         {
             var cameraId = (WebCameraId)comboBox.SelectedItem;
-            webCameraControl.StartCapture(cameraId);
-            
+            LearningImageOne.StartCapture(cameraId);
+
         }
-        public int TakeNumber()
-        {
-            int[] finalNum = new int[10];
-            for(int i = 0; i < COUNTNUMCARD;  i++)
-            {
-                Thread.Sleep(MSECONDNUMCARD);
-                using (var api = OcrApi.Create())
-                {
-                    api.SetVariable("tessedit_char_whitelist", "0123456789");
-                    api.Init(Languages.English);
-                    string plainText = api.GetTextFromImage((Bitmap)webCameraControl.GetCurrentImage());
-                    Console.WriteLine("prova" + plainText);
-                    for (int y = 0; y < 10; y++)
-                    {
-                        if(plainText.Contains(y.ToString()))
-                        {
-                            finalNum[y] += 1;
-                        }
-                    }
-                }
-            }
-            return finalNum.ToList().IndexOf(finalNum.Max());
-        }
+
+
+
+
+
+
+
+
+
+
+        //public int TakeNumber() {
+
+        //    //// Instantiates a client
+        //    //var client = ImageAnnotatorClient.Create();
+        //    //// Load the image file into memory
+        //    //var image = Google.Cloud.Vision.V1.Image.FromFile(@"c:\OCRTest\otto.jpg");
+        //    //// Performs label detection on the image file
+        //    //var response = client.DetectLabels(image);
+        //    //foreach (var annotation in response)
+        //    //{
+        //    //    if (annotation.Description != null)
+        //    //        Console.WriteLine(annotation.Description);
+        //    //}
+        //    int[] finalNum = new int[10];
+        //    for (int i = 0; i < COUNTNUMCARD; i++)
+        //    {
+        //        Thread.Sleep(MSECONDNUMCARD);
+        //        using (var api = OcrApi.Create())
+        //        {
+        //            api.SetVariable("tessedit_char_whitelist", "0123456789");
+        //            api.Init(Languages.English);
+        //            api.InputImage = OcrPix.FromBitmap((Bitmap)webCameraControl.GetCurrentImage());
+        //            api.SetImage(OcrPix.FromBitmap((Bitmap)webCameraControl.GetCurrentImage()));
+        //            api.Recognize();
+        //            //string plainText = api.GetTextFromImage((Bitmap)webCameraControl.GetCurrentImage());
+        //            Console.WriteLine("prova" + api.GetUtf8Text());
+        //            for (int y = 0; y < 10; y++)
+        //            {
+        //               if (api.GetUtf8Text().Contains(y.ToString()))
+        //               {
+        //                    finalNum[y] += 1;
+        //               }
+        //            }
+        //        }
+        //    }
+        //    return finalNum.ToList().IndexOf(finalNum.Max());
+        //}
 
         private void OnStopButtonClick(object sender, RoutedEventArgs e)
         {
-            webCameraControl.StopCapture();
+            LearningImageOne.StopCapture();
         }
-        public Bitmap CropBitmap(Bitmap bitmap, int cropX, int cropY, int cropWidth, int cropHeight)
-        {
-            Rectangle rect = new Rectangle(cropX, cropY, cropWidth, cropHeight);
-            Bitmap cropped = bitmap.Clone(rect, bitmap.PixelFormat);
-            return cropped;
-        }
-        private void OnImageButtonClick(object sender, RoutedEventArgs e)
-        {
-            Bitmap bmp = null;
-            bmp = (Bitmap)webCameraControl.GetCurrentImage();
-            Boolean IsColorFound = false;
-            int number = TakeNumber();
+        //public Bitmap CropBitmap(Bitmap bitmap, int cropX, int cropY, int cropWidth, int cropHeight)
+        //{
+        //    Rectangle rect = new Rectangle(cropX, cropY, cropWidth, cropHeight);
+        //    Bitmap cropped = bitmap.Clone(rect, bitmap.PixelFormat);
+        //    return cropped;
+        //}
+        //private void OnImageButtonClick(object sender, RoutedEventArgs e)
+        //{
+        //    Bitmap bmp = null;
+        //    bmp = (Bitmap)webCameraControl.GetCurrentImage();
+        //    Boolean IsColorFound = false;
+        //    int number = TakeNumber();
 
 
-            if (bmp != null)
-            {
-                //Iterate whole bitmap to findout the picked color
-                for (int i = 0; i < bmp.Height; i++)
-                {
-                    for (int j = 0; j < bmp.Width; j++)
-                    {
-                        //Get the color at each pixel
-                        Color now_color = bmp.GetPixel(j, i);
-                        //Compare Pixel's Color ARGB property with the picked color's ARGB property 
-                        if (now_color.R <= 50 && now_color.G >= 100 && now_color.B <= 50)
-                        {
-                            IsColorFound = true;
-                            MessageBox.Show("Number " + number + ", color green Found!");
-                            break;
-                        }
-                        if (now_color.R >= 100 && now_color.G >= 100 && now_color.B <= 50)
-                        {
-                            IsColorFound = true;
-                            MessageBox.Show("Number " + number + ", color yellow Found!");
-                            break;
-                        }
-                        if (now_color.R <= 50 && now_color.G <= 50 && now_color.B >= 100)
-                        {
-                            IsColorFound = true;
-                            MessageBox.Show("Number " + number + ", color blue Found!");
-                            break;
-                        }
-                        if (now_color.R >= 100 && now_color.G <= 50 && now_color.B <= 50)
-                        {
-                            IsColorFound = true;
-                            MessageBox.Show("Number " + number + ", color red Found!");
-                            break;
-                        }
-                    }
-                    if (IsColorFound == true)
-                    {
-                        break;
-                    }
-                }
-                if (IsColorFound == false)
-                {
-                    MessageBox.Show("Selected Color Not Found. " + number);
-                }
-            }
-            else
-            {
-                MessageBox.Show("Image is not loaded");
-            }
-            
-        }
+        //    if (bmp != null)
+        //    {
+        //        //Iterate whole bitmap to findout the picked color
+        //        for (int i = 0; i < bmp.Height; i++)
+        //        {
+        //            for (int j = 0; j < bmp.Width; j++)
+        //            {
+        //                //Get the color at each pixel
+        //                Color now_color = bmp.GetPixel(j, i);
+        //                //Compare Pixel's Color ARGB property with the picked color's ARGB property 
+        //                if (now_color.R <= 50 && now_color.G >= 100 && now_color.B <= 50)
+        //                {
+        //                    IsColorFound = true;
+        //                    MessageBox.Show("Number " + number + ", color green Found!");
+        //                    break;
+        //                }
+        //                if (now_color.R >= 100 && now_color.G >= 100 && now_color.B <= 50)
+        //                {
+        //                    IsColorFound = true;
+        //                    MessageBox.Show("Number " + number + ", color yellow Found!");
+        //                    break;
+        //                }
+        //                if (now_color.R <= 50 && now_color.G <= 50 && now_color.B >= 100)
+        //                {
+        //                    IsColorFound = true;
+        //                    MessageBox.Show("Number " + number + ", color blue Found!");
+        //                    break;
+        //                }
+        //                if (now_color.R >= 100 && now_color.G <= 50 && now_color.B <= 50)
+        //                {
+        //                    IsColorFound = true;
+        //                    MessageBox.Show("Number " + number + ", color red Found!");
+        //                    break;
+        //                }
+        //            }
+        //            if (IsColorFound == true)
+        //            {
+        //                break;
+        //            }
+        //        }
+        //        if (IsColorFound == false)
+        //        {
+        //            MessageBox.Show("Selected Color Not Found. " + number);
+        //        }
+        //    }
+        //    else
+        //    {
+        //        MessageBox.Show("Image is not loaded");
+        //    }
+
+        //}
     }
 }
